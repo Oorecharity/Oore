489263c3bfa174935ec7ba2b29d1940e656b6aaa
dabsmanuel <dabsmanuel5015@gmail.com>
2023-11-02 21:15:45 +0100

    added functionalities to the login and signup page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,83 @@
 "use client"
 
-import Image from "next/image"
-import Link from "next/link"
-import logo from "../assets/Group 2.png"
-import "./login.css"
+import React, { useState } from 'react';
+import Image from "next/image";
+import logo from "../assets/Group 2.png";
+import Link from 'next/link';
 
 const Login = () => {
+  // State to store login credentials and clear input fields
+  const [loginCredentials, setLoginCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  // State to store error messages
+  const [errorMessages, setErrorMessages] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Function to handle the form submission
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // Backend URL for login
+    const backendUrl = "https://api.ooreafrica.org/login"; // Replace with the actual backend URL
+
+    // Form data
+    const formData = new FormData(e.target);
+
+    try {
+      // Send a POST request to the backend
+      const response = await fetch(backendUrl, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        // Handle success, e.g., redirect to a new page
+        window.location.href = "/dashboard"; // Replace with the actual dashboard URL
+
+        // Clear input fields after successful login
+        setLoginCredentials({
+          email: "",
+          password: "",
+        });
+
+        // Clear error messages
+        setErrorMessages({
+          email: "",
+          password: "",
+        });
+      } else {
+        // Handle errors, e.g., show an error message
+        const errorData = await response.json();
+        setErrorMessages({
+          email: errorData.email || "",
+          password: errorData.password || "",
+        });
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  // Function to update login credentials
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginCredentials({
+      ...loginCredentials,
+      [name]: value,
+    });
+
+    // Clear the associated error message when the input changes
+    setErrorMessages({
+      ...errorMessages,
+      [name]: "",
+    });
+  };
+
   return (
     <div className="py-24">
       <div className="w-[85%] mx-auto bg-white p-16 rounded-md grid lg:grid-cols-2 grid-cols-1 gap-6">
@@ -21,16 +93,17 @@ const Login = () => {
             <h1 className="text-2xl font-bold text-center pt-8">Login your account</h1>
           </div>
           <div className="mt-8">
-            <form className="w-full max-w-md">
-              <h1 className="text-center text-3xl font-bold"></h1>
-
+            <form className="w-full max-w-md" onSubmit={handleLogin}>
               <input
                 type="text"
                 name="email"
                 id="email"
                 placeholder="Email address"
                 className="mt-6 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                value={loginCredentials.email}
+                onChange={handleInputChange}
               />
+              <span className="text-red-500">{errorMessages.email}</span>
 
               <input
                 type="password"
@@ -38,28 +111,31 @@ const Login = () => {
                 id="password"
                 placeholder="Password"
                 className="mt-8 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                value={loginCredentials.password}
+                onChange={handleInputChange}
               />
+              <span className="text-red-500">{errorMessages.password}</span>
 
               <button type="submit" className="mt-8 block w-full px-4 py-3 rounded-md text-sm hover:font-semibold bg-green hover:bg-green-600 transition all-ease duration-300 font-semibold">sign in</button>
               <button type="submit" className="mt-8 block w-full px-4 bg-gray-300 py-3 border border-gray-300 rounded-md text-sm font-semibold hover:bg-green transition all-ease duration-300">sign up</button>
 
-              <div className="text-center gray-300 text-sm mt-8"> 
+              <div className="text-center gray-300 text-sm mt-8">
                 <Link href="/">forgot password?</Link>
               </div>
             </form>
           </div>
         </div>
         <div>
-        <div className="lg:w-[75%] m-auto p-8 img h-[33rem] rounded">
-          <div className="pt-[22rem]">
-            <p className='text-[10px] text-center text-white'>Recent global estimates by UNICEF and partners indicate that at least 340 million children under 5 (one in two) suffer from hidden hunger. In the Western Africa UN sub-region 67% of children under 5 suffer from hidden hunger.</p>
-          </div>         
-        </div>
+          <div className="lg:w-[75%] m-auto p-8 img h-[33rem] rounded">
+            <div className="pt-[22rem]">
+              <p className='text-[10px] text-center text-white'>Recent global estimates by UNICEF and partners indicate that at least 340 million children under 5 (one in two) suffer from hidden hunger. In the Western Africa UN sub-region 67% of children under 5 suffer from hidden hunger.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
 
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,26 +1,109 @@
 "use client"
 
-import React, { useState } from 'react'; 
-import Image from "next/image"
-import logo from "../assets/Group 2.png"
-import "../login/login.css"
-import {country} from "../../../data"
+import React, { useState } from 'react';
+import Image from "next/image";
+import logo from "../assets/Group 2.png";
+import { country } from "../../../data";
 import Link from 'next/link';
 
 const Signup = () => {
-
-  // State to store the selected country
+  // State to store the selected country and clear input fields
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [inputFields, setInputFields] = useState({
+    username: "",
+    password: "",
+    phone: "",
+    email: "",
+    confirmPassword: "",
+  });
+
+  // State to store error messages
+  const [errorMessages, setErrorMessages] = useState({
+    username: "",
+    password: "",
+    phone: "",
+    email: "",
+    confirmPassword: "",
+  });
 
   // Function to handle the select change event
   const handleSelectChange = (event) => {
     setSelectedCountry(event.target.value);
   };
 
+  // Function to handle the form submission
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    // Backend URL for registration
+    const backendUrl = "https://api.ooreafrica.org/register"; // Replace with the actual backend URL
+
+    // Form data
+    const formData = new FormData(e.target);
+
+    try {
+      // Send a POST request to the backend
+      const response = await fetch(backendUrl, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        // Handle success, e.g., redirect to a new page
+        window.location.href = "/dashboard"; // Replace with the actual dashboard URL
+
+        // Clear input fields after successful submission
+        setInputFields({
+          username: "",
+          password: "",
+          phone: "",
+          email: "",
+          confirmPassword: "",
+        });
+
+        // Clear error messages
+        setErrorMessages({
+          username: "",
+          password: "",
+          phone: "",
+          email: "",
+          confirmPassword: "",
+        });
+      } else {
+        // Handle errors, e.g., show an error message
+        const errorData = await response.json();
+        setErrorMessages({
+          username: errorData.username || "",
+          password: errorData.password || "",
+          phone: errorData.phone || "",
+          email: errorData.email || "",
+          confirmPassword: errorData.confirmPassword || "",
+        });
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  // Function to update input field values
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputFields({
+      ...inputFields,
+      [name]: value,
+    });
+
+    // Clear the associated error message when the input changes
+    setErrorMessages({
+      ...errorMessages,
+      [name]: "",
+    });
+  };
+
   return (
-    <div className="login py-16 ">
+    <div className="login py-16">
       <div className="w-[85%] mx-auto bg-white p-8 rounded-md flex flex-col justify-center items-center">
-        <div className=''>
+        <div className="">
           <div className="mb-12 grid place-items-center">
             <Link href="/" className='mt-8'>
               <Image
@@ -30,18 +113,20 @@ const Signup = () => {
               />
             </Link>
             <h1 className="text-2xl font-bold text-left my-6">Create Your account</h1>
-
           </div>
           <div className="mt-8">
             <div className="lg:grid grid-cols-2 gap-6">
-              <form className="w-full max-w-md">
+              <form className="w-full max-w-md" onSubmit={handleSignup}>
                 <input
                   type="text"
                   placeholder="Username"
                   name="username"
                   id="username"
                   className="mt-3 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                  value={inputFields.username}
+                  onChange={handleInputChange}
                 />
+                <span className="text-red-500">{errorMessages.username}</span>
 
                 <input
                   type="password"
@@ -49,7 +134,10 @@ const Signup = () => {
                   id="password"
                   name="password"
                   className="mt-8 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                  value={inputFields.password}
+                  onChange={handleInputChange}
                 />
+                <span className="text-red-500">{errorMessages.password}</span>
 
                 <input
                   type="tel"
@@ -57,7 +145,10 @@ const Signup = () => {
                   name='phone'
                   placeholder="phone number"
                   className="mt-8 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                  value={inputFields.phone}
+                  onChange={handleInputChange}
                 />
+                <span className="text-red-500">{errorMessages.phone}</span>
               </form>
               <form className="w-full max-w-md">
                 <input
@@ -66,23 +157,29 @@ const Signup = () => {
                   name='email'
                   placeholder="Email Address"
                   className="mt-3 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                  value={inputFields.email}
+                  onChange={handleInputChange}
                 />
+                <span className="text-red-500">{errorMessages.email}</span>
 
                 <input
                   type="password"
-                  id='password'
-                  name='password'
+                  id='confirmPassword'
+                  name='confirmPassword'
                   placeholder="Confirm Password"
                   required
                   className="mt-8 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green"
+                  value={inputFields.confirmPassword}
+                  onChange={handleInputChange}
                 />
+                <span className="text-red-500">{errorMessages.confirmPassword}</span>
 
                 <select id="country" name="country" onChange={handleSelectChange} value={selectedCountry} className="mt-8 block w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green">
                   <option value="" required>Select country</option>
                   {
-                    country.map(({id, place}) =>{
-                      return(
-                        <option key={id} value={ place }>
+                    country.map(({ id, place }) => {
+                      return (
+                        <option key={id} value={place}>
                           {place}
                         </option>
                       )
@@ -92,15 +189,14 @@ const Signup = () => {
               </form>
             </div>
             <div className='grid place-items-center mt-8'>
-              <button type="submit" className="mt-4 block w-full max-w-md px-4 py-3 bg-green rounded-md text-sm hover:font-semibold transition all-ease duration-300">sign up</button>
-              <p className='text-gray-500 pt-6'>Already have an account? <span className='text-black hover:text-green transition all-ease duration-300'> <Link href="/login ">Login</Link></span></p>
+              <button type="submit" className="mt-4 block w-full max-w-md px-4 py-3 bg-green rounded-md text-sm hover:font-semibold transition all-ease duration-300">Sign up</button>
+              <p className='text-gray-500 pt-6'>Already have an account? <span className='text-black hover:text-green transition all-ease duration-300'> <Link href="/login">Login</Link></span></p>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
 
-export default Signup
+export default Signup;
